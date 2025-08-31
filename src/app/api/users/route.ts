f5ad9a232812ab5e2ec2aa43318ed65448151048
chritzadz <chrisdumanauw@gmail.com>
2025-09-01 01:13:29 +0800

    setted up connection disabling ssl

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -6,13 +6,20 @@ export async function POST(request: NextRequest) {
     const body = await request.json();
     const { name, email, password } = body;
     try {
+        const client = await pool.connect();
+        await client.query('SELECT 1');
+        client.release();
+
+        console.log('Database connected!');
+        
         const result = await pool.query(
             'INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING password, name, email',
             [name, email, password]
         );
+
         const newUser: User = result.rows[0];
         return NextResponse.json(newUser, { status: 201 });
     } catch (error) {
-        return NextResponse.json({ error: 'Failed to create user', details: error }, { status: 500 });
+        return NextResponse.json({ error: 'Failed to create user', details: (error as Error).message }, { status: 500 });
     }
 }
