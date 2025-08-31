301480f14d868f6e5cb548b5e4a2e8670247d9e3
chritzadz <chrisdumanauw@gmail.com>
2025-09-01 00:16:49 +0800

    set up api

---
+++ glasc-api/src/app/api/users/route.ts
@@ -0,0 +1,18 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { User } from '@/types/user';
+import pool from '@/app/db/db';
+
+export async function POST(request: NextRequest) {
+    const body = await request.json();
+    const { name, email, password } = body;
+    try {
+        const result = await pool.query(
+            'INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING password, name, email',
+            [name, email, password]
+        );
+        const newUser: User = result.rows[0];
+        return NextResponse.json(newUser, { status: 201 });
+    } catch (error) {
+        return NextResponse.json({ error: 'Failed to create user', details: error }, { status: 500 });
+    }
+}
