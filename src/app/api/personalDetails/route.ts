0b14d9282fea2889374a3c86299b99890d8525be
chritzadz <chrisdumanauw@gmail.com>
2025-09-05 16:50:00 +0800

    add error message

--- src/app/api/personalDetails/route.ts
+++ src/app/api/personalDetails/route.ts
@@ -22,6 +22,6 @@ export async function POST(request: NextRequest) {
 
         return NextResponse.json({ status: 201 });
     } catch (error) {
-        return NextResponse.json({ error: 'Failed to create user', details: (error as Error).message }, { status: 500 });
+        return NextResponse.json({ error: 'Failed posting personal details', details: (error as Error).message }, { status: 500 });
     }
 }
