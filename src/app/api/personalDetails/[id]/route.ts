c6bc22e77a6a2516499ad3b2cbbb26963c253874
chritzadz <chrisdumanauw@gmail.com>
2025-09-09 22:58:10 +0800

    using context

--- src/app/api/personalDetails/[id]/route.ts
+++ src/app/api/personalDetails/[id]/route.ts
@@ -1,17 +1,18 @@
-import { NextRequest, NextResponse } from 'next/server';
 import pool from '@/app/db/db';
+import { NextRequest, NextResponse } from 'next/server';
 
-
-export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
-    const userId = params.id;
+export async function GET(request: NextRequest, context: { params: { id: string } }) {
+    const userId = context.params.id;
     try {
         const task = await pool.query(
             `SELECT * FROM personal_details WHERE user_id = $1`,
             [userId]
         );
-
-        return NextResponse.json({task, status: 201 });
+        return NextResponse.json({ task }, { status: 200 });
     } catch (error) {
-        return NextResponse.json({ error: 'Failed posting personal details', details: (error as Error).message }, { status: 500 });
+        return NextResponse.json(
+            { error: 'Failed fetching personal details', details: (error as Error).message },
+            { status: 500 }
+        );
     }
 }
