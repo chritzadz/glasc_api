35aae4f2b6614c9bc1a5252d92fd49ad5eb39d35
christiandumanauw <christian.dumanauw@generali.com.hk>
2025-09-16 10:08:59 +0800

    update delete post and data type

--- src/app/api/skincareRoutine/route.ts
+++ src/app/api/skincareRoutine/route.ts
@@ -1,19 +1,33 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { User } from '@/types/user';
 import pool from '@/app/db/db';
 
 export async function POST(request: NextRequest) {
     const body = await request.json();
-    const { user_id, morning_routine, evening_routine } = body;
+    const { user_id, product, type } = body;
     try {
         await pool.query(
-            'INSERT INTO personal_skincare_routine (user_id, morning_routine, evening_routine) VALUES ($1, $2, $3) RETURNING *;',
-            [user_id, morning_routine, evening_routine]
+            'INSERT INTO personal_skincare_routine (user_id, product, type) VALUES ($1, $2, $3);',
+            [user_id, product, type]
         );
 
         return NextResponse.json({ status: 201 });
     } catch (error) {
         return NextResponse.json({ error: 'Failed to create skincare routine', details: (error as Error).message }, { status: 500 });
+    }
+}
+
+export async function DELETE(request: NextRequest) {
+    const body = await request.json();
+    const { user_id, product, type } = body;
+    try {
+        await pool.query(
+            'DELETE FROM personal_skincare_routine WHERE user_id = $1 AND product = $2 AND type = $3;',
+            [user_id, product, type]
+        );
+
+        return NextResponse.json({ status: 201 });
+    } catch (error) {
+        return NextResponse.json({ error: 'Failed to delete skincare routine', details: (error as Error).message }, { status: 500 });
     }
 }
 
